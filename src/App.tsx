752989d1fd48da6aa4bf0ef37aff575ac5119d07
Bohdan <felbohdan@gmail.com>
2026-05-18 02:51:00 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { useState } from 'react';
+import { Movie } from './types/Movie';
+
+
 
 export const App = () => {
+  const [visibleMovies,setVisibleMovies] = useState(moviesFromServer);
+  const [formKey, setFormKey] = useState(0);
+
+  function addMovie(movie:Movie){
+    setVisibleMovies([...visibleMovies,movie])
+    setFormKey(formKey + 1)
+  }
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie key = {formKey} onAdd={addMovie}  />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,105 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
+type Props = {
+  onAdd: (movie:Movie)=>void
+};
+
+export const NewMovie: React.FC<Props> = ({onAdd}) => {
   const [count] = useState(0);
+  const [formData,setFormData] = useState({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+  })
+  let isFormFull = true;
+
+  for (const key in formData) {
+    const currentKey = key as keyof typeof formData;
+    if(currentKey !== 'description') {
+      if(formData[currentKey] === '') {
+        isFormFull = false;
+      } 
+    }
+  }
+
+const handleFieldChange = (fieldName: keyof typeof formData) => (value: string) => {
+  setFormData(prev => ({
+    ...prev, 
+    [fieldName]: value,
+  }));
+  
+};
+
+const handleSubmit = (e:React.FormEvent) => {
+  e.preventDefault();
+  if(true){
+
+  }
+  onAdd(formData)
+  resetForm();
+}
+
+function resetForm() {
+   setFormData({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+}
 
   return (
-    <form className="NewMovie" key={count}>
+    <form 
+      className="NewMovie" 
+      key={count}
+      onSubmit={handleSubmit}
+      >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={formData.title}
+        onChange={handleFieldChange('title')}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField 
+      name="description" 
+      label="Description" 
+      value={formData.description}
+      onChange={handleFieldChange('description')}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField 
+      name="imgUrl" 
+      label="Image URL" 
+      value={formData.imgUrl}
+      onChange={handleFieldChange('imgUrl')}
+      required 
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField 
+      name="imdbUrl" 
+      label="Imdb URL" 
+      value={formData.imdbUrl}
+      onChange={handleFieldChange('imdbUrl')}
+      required 
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField 
+      name="imdbId" 
+      label="Imdb ID" 
+      value={formData.imdbId}
+      onChange={handleFieldChange('imdbId')}
+      required 
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +107,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled = {!isFormFull}
           >
             Add
           </button>
